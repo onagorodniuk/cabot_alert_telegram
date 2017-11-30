f0570566b6b45821a1a9d5e911e8b51fc4dd08a4
onagorodniuk <gagarinq9@gmail.com>
2017-11-30 17:35:18 +0200

    Create 0001_initial.py

---
+++ cabot_alert_telegram/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cabotapp', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SnsAlert',
+            fields=[
+                ('alertplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPlugin')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cabotapp.alertplugin',),
+        ),
+        migrations.CreateModel(
+            name='SnsAlertUserData',
+            fields=[
+                ('alertpluginuserdata_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cabotapp.AlertPluginUserData')),
+                ('sns_arn', models.CharField(max_length=1000, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cabotapp.alertpluginuserdata',),
+        ),
+    ]
